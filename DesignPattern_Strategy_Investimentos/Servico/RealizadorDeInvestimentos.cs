7136c01af07079395b10d4272cb831505f3d4af4
MarcoADeCSilva <marcoacsilva@live.com>
2021-11-17 23:12:42 -0300

    Adicionado o tipo de investimento.

--- DesignPattern_Strategy_Investimentos/Servico/RealizadorDeInvestimentos.cs
+++ DesignPattern_Strategy_Investimentos/Servico/RealizadorDeInvestimentos.cs
@@ -8,12 +8,12 @@ namespace DesignPattern_Strategy_Investimentos.Servico
     {
         public void CalculaLucroAposInvestimento(ContaBancaria contaBancaria, IInvestimento investimento, double chanceDeAcontecer)
         {
-            Console.WriteLine($"O Saldo da sua conta bancaria antes do seu investimento é R${contaBancaria.Saldo}");
+            Console.WriteLine($"O Saldo da sua conta bancaria antes do seu investimento é R${contaBancaria.Saldo}.");
 
             double lucroObtidocomInvestimento = (investimento.CalculaLucro(contaBancaria, chanceDeAcontecer) * 0.75);
             double total = contaBancaria.Saldo + lucroObtidocomInvestimento;
 
-            Console.WriteLine($"Após o investimento {investimento}, seu Saldo é R${total}");
+            Console.WriteLine($"Após o investimento {investimento.GetType().Name}, seu Saldo é R${total}.");
         }
     }
 }
